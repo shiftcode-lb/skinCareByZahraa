cbc8e0119fad332ae34cb8c4a479cd0cc2bc953c
Ali Issa <alyy.issa01@gmail.com>
2025-08-27 16:50:28 +0300

    Adding React in main.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets'
 const Home = () => {
   return (
     <div className='text-2xl font-bold'>
-      <h1>asdadsads</h1>
+      <h1 className='font-black'>asdadsads</h1>
     </div>
   )
 }
