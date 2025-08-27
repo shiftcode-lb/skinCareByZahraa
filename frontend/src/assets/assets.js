326ea371316303670e3e7f3f29c23e42a8e80aea
Ali Issa <alyy.issa01@gmail.com>
2025-08-27 16:46:14 +0300

    Added initial files

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,5 @@
+export const name = "ali"
+
+export const assets = [
+    
+]
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Home = () => {
+  return (
+    <div className='text-2xl font-bold'>
+      <h1>asdadsads</h1>
+    </div>
+  )
+}
+
+export default Home
